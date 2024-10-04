8e237dd614a7bb76b1f585f6375773f58dc28e7e
gaelvi <165824660+gaelvi@users.noreply.github.com>
2024-10-04 15:23:21 -0300

    salvaaaaa

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criacartao(
+    'stitch',
+    'Qual é o nome da familia do stitch?',
+    'ohana'
+)
+criacartao(
+'BMW',
+'Qual BMW é a mais potente?',
+'BMW M3 CS',
+)
+criacartao(
+    
+
+
+
+
+
+
+)
+
+
+
